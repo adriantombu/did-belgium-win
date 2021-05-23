a52628fd65538150da13da3b19f0da1270920234
Adrian Tombu <adrian@otso.fr>
2021-05-23 13:21:57 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Did Belgium Win?
+
+A twitter bot counting the days since the last trophy of the [Belgian Football team](https://twitter.com/belreddevils).
+
+You can follow us on Twitter : [https://twitter.com/BelgiumWon](https://twitter.com/BelgiumWon)
+
+### Red Devils' palmares
+
+- First place during the 1920 Olympic Games in Belgium
+- Second place during the 1980 UEFA European Championship
+- Third place during the 2018 World Cup in Russia with [one of the most beautiful counter-attack ever played](https://www.youtube.com/watch?v=iTlfLJCg6W4) (it stills gives me the chills)
+- Third place during the 1972 UEFA European Championship
+- Third place during the 1900 Olympic Games in France
+- Fourth place during the 1986 World Cup in Mexico
+- Fourth place during the 2008 Olympic Games in Beijing
+
+*Sources: [https://en.wikipedia.org/wiki/Belgium_national_football_team#Competitive_record](https://en.wikipedia.org/wiki/Belgium_national_football_team#Competitive_record)*
---
+++ main.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const Twitter = require('twitter-lite')
+const dayjs = require('dayjs')
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
+
+const main = async () => {
+  try {
+    const body = {
+      status: `It has been ${formatDate()} since the @BelRedDevils won their last trophy, the 1920 Olympic Games in Antwerp, Belgium\n#DEVILTIME #EURO2020 #COMEONEBELGIUM 🇧🇪`,
+      place_id: '0653bb913c88c1ea',
+    }
+    const tweet = await client.post('statuses/update', body)
+    console.log(tweet)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const formatDate = () => {
+  const now = dayjs()
+  const lastWin = dayjs('1920-09-02')
+  const totalDays = now.diff(lastWin, 'days').toLocaleString('en-US')
+  const y = now.diff(lastWin, 'years')
+  const m = now.subtract(y, 'years').diff(lastWin, 'months')
+  const d = now.subtract(y, 'years').subtract(m, 'months').diff(lastWin, 'days')
+
+  let res = `${totalDays} days (${y} years`
+  if (m) res += `, ${m} month${m > 1 ? 's' : ''}`
+  if (d) res += `, ${d} day${d > 1 ? 's' : ''}`
+  res += ')'
+
+  return res
+}
+
+const getBrusselsId = async () => {
+  const location = await client.get('geo/search', {
+    lat: 50.846799073627025,
+    long: 4.3524356783587,
+    granularity: 'city',
+    max_results: 10,
+  })
+
+  console.log(location.result.places)
+}
+
+main()
